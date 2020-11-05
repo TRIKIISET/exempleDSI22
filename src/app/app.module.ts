ca2cfe44827d95015c23f88e07308f25c0120332
Amel TRIKI <amel.triki.iset@gmail.com>
2020-11-05 15:58:19 +0100

    composant ajouté

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { EssaiComponent } from './essai/essai.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EssaiComponent
   ],
   imports: [
     BrowserModule,
