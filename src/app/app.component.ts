1af7375f5f9390393f304b77e78e7969964f5760
Amel TRIKI <amel.triki.iset@gmail.com>
2020-11-05 14:52:50 +0100

    Partie 1 démo fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monProjet';
+  etudiants = [
+    {nom:'Amir', moyenne:15.5, ville:'Tunis'},
+    {nom:'Mouna', moyenne:14.2, ville:'Sfax'},
+    {nom:'Sami', moyenne:8.75, ville:'Sousse'},
+    {nom:'Sameh', moyenne:12.42, ville:'Tunis'},
+    {nom:'Mounir', moyenne:13.87, ville:'Nabeul'},
+    {nom:'Siwar', moyenne:7.9, ville:'Tunis'},
+    {nom:'Karim', moyenne:12.56, ville:'Sfax'},
+    {nom:'Ahlem', moyenne:9.96, ville:'Sousse'}
+  ]
 }
